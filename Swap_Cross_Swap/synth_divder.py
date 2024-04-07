53bc68d4f7ae5293571b1428adcc458792973c4c
Freeman <107298762+Freemandaily@users.noreply.github.com>
2024-04-07 11:07:22 +0100

    Update synth_divder.py

--- Swap_Cross_Swap/synth_divder.py
+++ Swap_Cross_Swap/synth_divder.py
@@ -4,12 +4,12 @@ import subprocess,time
 print('updated 1')
 take_action =  True
 while take_action:
-    subprocess.Popen(['python3','mainSyth.py','arbitrumBot_1'])
+    subprocess.Popen(['python3','mainSynth.py','arbitrumBot_1'])
     time.sleep(5)
-    subprocess.Popen(['python3','mainSyth.py','arbitrumBot_2'])
-    time.sleep(10)
-    subprocess.Popen(['python3','mainSyth.py','arbitrumBot_3'])
-    time.sleep(10)
-    subprocess.Popen(['python3','mainSyth.py','arbitrumBot_4'])
+    #subprocess.Popen(['python3','mainSynth.py','arbitrumBot_2'])
+    #time.sleep(10)
+    #subprocess.Popen(['python3','mainSynth.py','arbitrumBot_3'])
+    #time.sleep(10)
+    #subprocess.Popen(['python3','mainSynth.py','arbitrumBot_4'])
     print('WAITING FOR THE NEXT 30 MINUTE')
     time.sleep(1800)

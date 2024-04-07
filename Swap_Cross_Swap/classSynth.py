c7226560bd5345dbbc64e018c7dc59e1f0cbedf8
Freeman <107298762+Freemandaily@users.noreply.github.com>
2024-04-07 10:24:40 +0100

    Add files via upload

---
+++ Swap_Cross_Swap/classSynth.py
@@ -0,0 +1,167 @@
+from web3 import *
+import time,sys,os
+import telegram,asyncio
+from telegram.constants import ParseMode
+import synth_Abi
+
+
+connect = Web3(Web3.HTTPProvider('https://arbitrum-sepolia.infura.io/v3/d049a2241d97413da17d774171eb0edb'))
+
+SyUSD_CONTRACT = connect.eth.contract(address=connect.to_checksum_address(synth_Abi.SyUSD_ADDRESS),abi=synth_Abi.BASIC_TOKEN_ABI)
+
+
+
+class SyTHNR:
+    def __init__(self,priv,amountIn) -> None:
+        self.priv = priv
+        self.account = Account.from_key(self.priv)
+        self.SyUSD_sourceCurrencyKey = '0x7355534400000000000000000000000000000000000000000000000000000000'
+        self.amountIn = int(amountIn*10**18)
+        self.amountOut = 0
+        self.bridgeName = '0x4c617965725a65726f0000000000000000000000000000000000000000000000'
+        self.chainId = 0
+        self.erc20Payment = False
+
+
+    # Same Chain Swapping
+    def SameChainSwap(self,connect,Router,destinationCurrencyKey):
+        user_balance = SyUSD_CONTRACT.functions.balanceOf(self.account.address).call()
+        
+        if user_balance >= self.amountIn :
+            try:
+                transaction = Router.functions.exchangeAtomically(
+                    self.SyUSD_sourceCurrencyKey,
+                    self.amountIn,
+                    destinationCurrencyKey,
+                    self.amountOut,
+                    self.bridgeName,
+                    self.chainId,
+                    self.erc20Payment
+                ).build_transaction({
+                    'from': self.account.address,
+                    'gas': 500000,
+                    'gasPrice': connect.to_wei('0.2','gwei'),
+                    'nonce': connect.eth.get_transaction_count(self.account.address)
+                })
+
+                sign_trx = connect.eth.account.sign_transaction(transaction,self.priv)
+                send = connect.eth.send_raw_transaction(sign_trx.rawTransaction)
+                print(f'The transaction hash for Same ChainSwap is: {send.hex()}')
+                return send
+            except Exception as e:
+                #Alert
+                action = 'SameChainSwap'
+                self.failedTrade(e,action)
+                print(f'The problem is from {e}')
+        else :
+            # Alert
+            error = 'YOU DONT HAS ENOUGH SyUSD BALANCE'
+            action = 'SameChainSwap'
+            self.failedTrade(error,action)
+
+    # For Cross Chain Swapping
+    def CrossChainSwap(self,connect,Router,destinationCurrencyKey,chainId):
+        user_balance = SyUSD_CONTRACT.functions.balanceOf(self.account.address).call()
+
+        if user_balance >= self.amountIn :
+            try:
+                transaction = Router.functions.exchangeAtomically(
+                        self.SyUSD_sourceCurrencyKey,
+                        self.amountIn,
+                        destinationCurrencyKey,
+                        self.amountOut,
+                        self.bridgeName,
+                        chainId,
+                        self.erc20Payment
+                    ).build_transaction({
+                        'from': self.account.address,
+                        'value': connect.to_wei('0.0007','ether'),
+                        'gas': 700000,
+                        'gasPrice': connect.to_wei('1.6','gwei'),
+                        'nonce': connect.eth.get_transaction_count(self.account.address)
+                    })
+                
+                sign_trx = connect.eth.account.sign_transaction(transaction,self.priv)
+                send = connect.eth.send_raw_transaction(sign_trx.rawTransaction)
+                print(f'The transaction hash for CrossChainSwap is: {send.hex()}')
+                return send
+            except  Exception as e:
+                # Alert
+                action = 'CrossChainSwap'
+                self.failedTrade(e,action)
+                print(f'The issue is from {e}')
+        else :
+             # Alert
+            error = 'YOU DONT HAS ENOUGH SyUSD BALANCE'
+            action = 'CrossChainSwap'
+            self.failedTrade(error,action)
+
+    # For Bridging
+    def BridgeSwap(self,connect,Router,chainId,amount_In):
+        amountIn = int(amount_In*10**18)
+        user_balance = SyUSD_CONTRACT.functions.balanceOf(self.account.address).call()
+
+        if user_balance >= amountIn:
+            try:
+                transaction = Router.functions.bridgeSynth(
+                    self.account.address,
+                    self.SyUSD_sourceCurrencyKey,
+                    amountIn,
+                    self.bridgeName,
+                    chainId,
+                    self.erc20Payment
+                ).build_transaction({
+                    'from': self.account.address,
+                    'value': connect.to_wei('0.0007','ether'),
+                    'gas': 700000,
+                    'gasPrice': connect.to_wei('1.6','gwei'),
+                    'nonce': connect.eth.get_transaction_count(self.account.address)
+
+                })
+
+                sign_trx = connect.eth.account.sign_transaction(transaction,self.priv)
+                send = connect.eth.send_raw_transaction(sign_trx.rawTransaction)
+                print(f'The Transaction hash for Bridging is {send.hex()}')
+                return send
+            except Exception as e:
+                # Alert
+                action = 'bridge'
+                self.failedTrade(e,action)
+                print(f'The issue is from {e}')
+        else:
+              # Alert
+            error = 'YOU DONT HAS ENOUGH SyUSD BALANCE'
+            action = 'Bridge'
+            self.failedTrade(error,action)
+
+    
+    # Alert
+    def Alert(self,hash,walletNumber):
+        bot_token = '6348189496:AAEa_9d8h-FPAG_OICpIq6rV8E0k3roPhvY'
+        hash_link = f'https://sepolia.arbiscan.io/tx/{hash.hex()}'
+        trade_info =  f' ALL SYNTHR TRANSCTIONS SUCCESFULLY POSTED \n\n'\
+                      f'WALLET: {walletNumber}\n\n'\
+                      f'CLICK <a href="{hash_link}">HASH </a> TO VIEW TRANSACTION\n\n'
+        async def main():
+            try:
+                bot = telegram.Bot(bot_token)
+            except:
+                bot = telegram.Bot(bot_token)
+            async with bot:
+                await bot.send_message(text=trade_info,parse_mode=ParseMode.HTML,chat_id=963648721)
+        if __name__!='__main__':
+            asyncio.run(main())
+      
+   # For failed Trade
+    def failedTrade(self,e,action):
+        bot_token = '6344573464:AAF_dIkl-hJ5aFT_f0IbUMCmwtOhIm41tvc'
+        async def main():
+            try:
+                bot = telegram.Bot(bot_token)
+            except:
+                bot = telegram.Bot(bot_token)
+            async with bot:
+                await bot.send_message(text=f'UNABLE TO POST TRANSACTION\n\nActiom:{action}\n\nERROR: {e}',chat_id=963648721)
+        if __name__!='__main__':
+            asyncio.run(main())
+
---
+++ Swap_Cross_Swap/mainSyth.py
@@ -0,0 +1,125 @@
+# This program is for synth testnet
+
+from web3 import *
+import time,os,sys,random
+import synth_Abi
+from classSynth import SyTHNR
+
+print('UPDATED_4')
+connect = Web3(Web3.HTTPProvider('https://arbitrum-sepolia.infura.io/v3/d049a2241d97413da17d774171eb0edb'))
+print(connect.is_connected())
+
+# TOKEN AND CONTRACTS USED 
+SWAP_ROUTER = connect.eth.contract(address=connect.to_checksum_address(synth_Abi.SWAP_ROUTER_ADRESS),abi=synth_Abi.SWAP_ROUTER_ABI)
+BRIGDE_ROUTER = connect.eth.contract(address=connect.to_checksum_address(synth_Abi.BRIDGE_ROUTER_ADDRESS),abi=synth_Abi.BRIDGE_ROUTER_ABI)
+
+SyUSD_sourceCurrencyKey = '0x7355534400000000000000000000000000000000000000000000000000000000'
+SyBNB_destinationCurrencyKey = '0x73424e4200000000000000000000000000000000000000000000000000000000'
+SyAVAX_destinationCurrencyKey = '0x7341564158000000000000000000000000000000000000000000000000000000'
+SyMATIC_destinationCurrencyKey = '0x734d617469630000000000000000000000000000000000000000000000000000'
+
+amount_In_list = [40,20,40,30]
+destination_list = [ SyBNB_destinationCurrencyKey,SyAVAX_destinationCurrencyKey,SyMATIC_destinationCurrencyKey]
+
+hashes = [ ]
+def Action(priv,wallet_number):
+    number_of_swap = random.choice([3,3])
+    destination = random.sample(destination_list,number_of_swap)
+    amountIn = random.choice(amount_In_list)
+    processor = SyTHNR(priv,amountIn) 
+
+    # For same Chain Swap
+    def SwapSameChain():
+        for chain in destination:
+            try:
+                hash = processor.SameChainSwap(connect,SWAP_ROUTER,chain)
+                print('Waiting for 10 before swapping another')
+                hashes.append(hash)
+                time.sleep(5)
+            except Exception as e:
+                print(f'The issue is from {e}')
+            
+
+    # For Cross Chain Swapping
+    def CrossChain():
+        for chain in destination_list :
+            try:
+                if chain == SyBNB_destinationCurrencyKey:
+                    chainId = 10102
+                    hash = processor.CrossChainSwap(connect,SWAP_ROUTER,chain,chainId)
+                    hashes.append(hash)
+                elif chain == SyAVAX_destinationCurrencyKey:
+                    chainId = 10106
+                    hash = processor.CrossChainSwap(connect,SWAP_ROUTER,chain,chainId)
+                    hashes.append(hash)
+                elif chain == SyMATIC_destinationCurrencyKey:
+                    chainId = 10109
+                    hash = processor.CrossChainSwap(connect,SWAP_ROUTER,chain,chainId)
+                    hashes.append(hash)
+                print('Waiting for 10 seconds before swapping another')
+                time.sleep(5)
+            except:
+                pass
+            #return hash
+    
+    # Bridging
+    def Bridge():
+        for chain in destination_list:
+            amount_in_list = [20,40,20,30]
+            try:
+                amount_in = random.choice(amount_in_list)
+                if chain == SyBNB_destinationCurrencyKey:
+                    chainId = 10102
+                    hash = processor.BridgeSwap(connect,BRIGDE_ROUTER,chainId,amount_in)
+                    hashes.append(hash)
+                elif chain == SyAVAX_destinationCurrencyKey:
+                    chainId = 10106
+                    hash = processor.BridgeSwap(connect,BRIGDE_ROUTER,chainId,amount_in)
+                    hashes.append(hash)
+                elif chain == SyMATIC_destinationCurrencyKey:
+                    chainId = 10109
+                    hash = processor.BridgeSwap(connect,BRIGDE_ROUTER,chainId,amount_in)
+                    hashes.append(hash)
+                print('Waiting for 10 seconds before swapping another')
+                time.sleep(5)
+            except:
+                pass
+            #return hash
+
+    # This is where to chooose which one action to take
+    action_initials =  [0,1,2]
+    random_initials = random.sample(action_initials,3)
+    for initial in random_initials:
+        if initial == 0 :
+            SwapSameChain()
+        elif initial == 1 :
+            CrossChain()
+        elif initial == 2 :
+            Bridge()
+
+    hash = random.choice(hashes)    
+    processor.Alert(hash,wallet_number)
+ 
+
+# Taking Command From The Subprocess
+wallet_command = sys.argv[1] 
+if wallet_command == 'arbitrumBot_1':
+    priv = os.environ.get('arbitrumBot_1')
+    wallet_number = 1
+    Action(priv,wallet_number)
+elif wallet_command == 'arbitrumBot_2':
+    priv = os.environ.get('arbitrumBot_2')
+    wallet_number = 2
+    Action(priv,wallet_number)
+elif wallet_command == 'arbitrumBot_3':
+    priv = os.environ.get('arbitrumBot_3')
+    wallet_number = 3
+    Action(priv,wallet_number)
+elif wallet_command == 'arbitrumBot_4':
+    priv = os.environ.get('arbitrumBot_4')
+    wallet_number = 4
+    Action(priv,wallet_number)
+
+
+
+
